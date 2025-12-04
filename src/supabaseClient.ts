6451cce0f4195dab8fd8f245f2bfe9b45f4d8044
RistinAndini78 <ristinristin798@gmail.com>
2025-12-04 15:15:30 +0700

    database

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  "https://axlfkfybsryjhhlzthen.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4bGZrZnlic3J5amhobHp0aGVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3ODA0NTUsImV4cCI6MjA4MDM1NjQ1NX0.i0qBpDOg4w9JuZH_xWKnsHNdDWdvcSia_gTqEIMmZks"
-);
+  "https://uvtdqgjrzitkszctwhag.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2dGRxZ2pyeml0a3N6Y3R3aGFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MTU2NzMsImV4cCI6MjA4MDM5MTY3M30.ZxM_d1pWc2i2NVKpT76o_-uLri4F8JRiEvmhEySnds8");
